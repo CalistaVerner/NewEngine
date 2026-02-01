862a4ab06783a6892db8eec9a66e46be037c86d6
CalistaFox <miomoor182@gmail.com>
2026-02-01 22:17:07 +0300

    DDS importer

---
+++ neocore2/crates/newengine-import-dds/src/plugins.rs
@@ -0,0 +1,18 @@
+#![forbid(unsafe_op_in_unsafe_fn)]
+
+use abi_stable::derive_macro_reexports::PrefixTypeTrait;
+use abi_stable::export_root_module;
+use abi_stable::sabi_trait::TD_Opaque;
+
+use newengine_plugin_api::{PluginModuleDyn, PluginModule_TO, PluginRootV1, PluginRootV1Ref};
+
+use crate::module::DdsImporterPlugin;
+
+#[export_root_module]
+pub fn export_plugin_root() -> PluginRootV1Ref {
+    PluginRootV1 { create: create_module }.leak_into_prefix()
+}
+
+extern "C" fn create_module() -> PluginModuleDyn<'static> {
+    PluginModule_TO::from_value(DdsImporterPlugin::default(), TD_Opaque)
+}
